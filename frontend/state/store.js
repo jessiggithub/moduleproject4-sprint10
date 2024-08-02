801f5b1aff7207c96c2e0be7e2b1786d9fac0639
Jessica Garcia <jessicagarcia9696@gmail.com>
2024-08-02 17:10:44 -0400

    finished

--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import quotesReducer from './quotesSlice'
+import { quotesApi } from './quotesApi'
 
 export const store = configureStore({
   reducer: {
     quotesState: quotesReducer,
-  }
+    [quotesApi.reducerPath]: quotesApi.reducer,
+  },
+  niddleware: getDefault => getDefault().concat(
+    quotesApi.middleware,
+  ),
 })
